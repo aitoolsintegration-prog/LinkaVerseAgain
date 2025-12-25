0b1a89dbfb59e449fec97ae1c53a20c0299d58fc
aitoolsintegration <aitoolsintegration@gmail.com>
2025-12-25 22:23:22 +1100

    Version1: AndroidManifest.xml, activity_main.xml, MainActivity.java

---
+++ app/src/main/java/com/thematicbiblestudy/linkaverse/MainActivity.java
@@ -0,0 +1,39 @@
+package com.thematicbiblestudy.linkaverse;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    String websiteURL = "https://thematic-bible-study-1069205335378.us-west1.run.app/"; // sets web url
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Webview stuff
+        webview = findViewById(R.id.webView);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl(websiteURL);
+        webview.setWebViewClient(new WebViewClientDemo());
+
+    }
+
+    private class WebViewClientDemo extends WebViewClient {
+        @Override
+        //Keep webview in app when clicking links
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+}
+
